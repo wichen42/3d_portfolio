7530c33a4a4e83d997a944c5cd21cc840309bda4
Wilson Chen <wilsonchen901@gmail.com>
2023-03-10 16:37:56 -0500

    complete hero section

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React.js Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Keyboard Enthusiast",
       icon: creator,
     },
   ];
